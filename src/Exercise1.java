61e03e039aef2ba184e9325629e955a2336efb81
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-07 11:44:29 +0700

    add comment

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Exercise1 {
+    // [Thực hành] Static Method
     public static void main(String[] args) {
         Student s1 = new Student(111, "Hoang");
         Student s2 = new Student(222, "Khanh");
